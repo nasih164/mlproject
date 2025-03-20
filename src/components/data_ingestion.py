073762d27925d71bb2be7364cf631b76d3c9d314
nasih164 <nasih1641@gmail.com>
2025-03-20 16:29:22 +0530

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,7 +1,5 @@
 import sys
-print(sys.path)
 import os
-import sys
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
@@ -12,6 +10,8 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 
 
 @dataclass
@@ -56,4 +56,7 @@ if __name__=="__main__":
   train_data,test_data=obj.initiate_data_ingestion()
 
   data_transformation=DataTransformation()
-  data_transformation.initiate_data_transformation(train_data,test_data)
+  train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+  modeltrainer=ModelTrainer()
+  print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
